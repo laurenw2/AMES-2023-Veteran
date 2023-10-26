85e84aca6e0b9578256df810e5f7f7439c23e235
laurenw2 <you@example.com>
2023-10-26 15:28:47 -0600

    mostly completed bones of pos control

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,49 +4,96 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.TalonFXSensorCollection;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.DoingThingsMotorIDs;
+import frc.robot.Constants.MotorIDs;
+import frc.robot.Constants.MotorPIDConstants;
+import frc.robot.Constants.MotorSpeedsMaxes;
+import frc.robot.Constants.PositionValues;
 
 public class ArmSubsystem extends SubsystemBase {
     //init stuff
       TalonFX armSwingMotor;
-      TalonFX wristMotor;
+      TalonFXSensorCollection armSwingEnc;
       CANSparkMax armExtendMotor1;
       CANSparkMax armExtendMotor2;
+      SparkMaxPIDController armExtendPID;
+      RelativeEncoder armExtendEncoder;
   
   public ArmSubsystem() {
     //motors/encoders
-      armSwingMotor = new TalonFX(DoingThingsMotorIDs.armSwingMotorID);
-      wristMotor = new TalonFX(DoingThingsMotorIDs.wristMotorID);
-      armExtendMotor1 = new CANSparkMax(DoingThingsMotorIDs.armExtendMotorID1, MotorType.kBrushless);
-      armExtendMotor2 = new CANSparkMax(DoingThingsMotorIDs.armExtendMotorID2, MotorType.kBrushless);
+      armSwingMotor = new TalonFX(MotorIDs.armSwingMotorID);
+      armExtendMotor1 = new CANSparkMax(MotorIDs.armExtendMotorID1, MotorType.kBrushless);
+      armExtendMotor2 = new CANSparkMax(MotorIDs.armExtendMotorID2, MotorType.kBrushless);
+      armExtendEncoder = armExtendMotor1.getEncoder();
+      armExtendPID = armExtendMotor1.getPIDController();
+
+      armExtendMotor2.follow(armExtendMotor1);
 
     //config PID
+      armSwingMotor.config_kF(0, MotorPIDConstants.armSwingMotorkF);
+      armSwingMotor.config_kP(0, MotorPIDConstants.armSwingMotorkP);
+      armSwingMotor.config_kI(0, MotorPIDConstants.armSwingMotorkI);
+      armSwingMotor.config_kD(0, MotorPIDConstants.armSwingMotorkD);
+
+      armExtendPID.setFF(MotorPIDConstants.armExtendkF);
+      armExtendPID.setP(MotorPIDConstants.armExtendkP);
+      armExtendPID.setI(MotorPIDConstants.armExtendkI);
+      armExtendPID.setD(MotorPIDConstants.armExtendkD);
 
     //config max output, safety
-
+      armSwingMotor.configClosedloopRamp(MotorSpeedsMaxes.armSwingClosedRamp);
+      armSwingMotor.configClosedLoopPeakOutput(0, MotorSpeedsMaxes.armSwingClosedMaxOutput);
+      armExtendMotor1.setClosedLoopRampRate(MotorSpeedsMaxes.armExtendClosedRamp);
+      armExtendMotor2.setClosedLoopRampRate(MotorSpeedsMaxes.armExtendClosedRamp);
+      armExtendPID.setOutputRange(-MotorSpeedsMaxes.armExtendClosedMaxOutput, MotorSpeedsMaxes.armExtendClosedMaxOutput);
 
   }
 
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("Arm Swing Encoder Value", armSwingEnc.getIntegratedSensorPosition());
+    SmartDashboard.putNumber("Arm Extend Encoder Value", armExtendEncoder.getPosition());
   }
 
   public void resetEncoders(){
-
+    armSwingEnc.setIntegratedSensorPosition(0, 15);
+    armExtendEncoder.setPosition(0);
   }
 
-  public void setLow(){}
+  public void setSwingHome(){
+    //set swing motor to home encoder position
+    armSwingMotor.set(ControlMode.Position, PositionValues.armSwingHomeVal);
+  }
 
-  public void setMid(){}
+  public void setSwingScore(){
+    //set swing motor to score encoder position
+    armSwingMotor.set(ControlMode.Position, PositionValues.armSwingScoreVal);
+  }
 
-  public void setHigh(){}
+  public void setSwingClimb(){
+    //set swing motor to climb encoder position
+    armSwingMotor.set(ControlMode.Position, PositionValues.armSwingClimbVal);
+  }
 
-  public void setHome(){}
+  public void setExtendIn(){
+    //set extend motor to "in" position
+    armExtendPID.setReference(PositionValues.armExtendInVal, ControlType.kPosition);
+  }
+
+  public void setExtendOut(){
+    //set extend motor to "out" position
+    armExtendPID.setReference(PositionValues.armExtendOutVal, ControlType.kPosition);
+  }
 
   public void moveManual(){}
 
